b90d03621da7f11deee897c408098b8fa5b723ee
koke <niwaniwa@users.noreply.github.com>
2024-05-08 18:44:28 +0900

    add jinsmeme connection process

---
+++ Assets/MimiCast/Scripts/Adapter/Connecter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MimiCast.Scripts.Infrastructure;
+using UniRx;
+using UnityEngine;
+
+namespace MimiCast.Scripts.Adapter
+{
+    public class Connecter : MonoBehaviour
+    {
+        [SerializeField] private Device device;
+        
+        
+        private void Start()
+        {
+            device.OnJinsDataReceive.Subscribe(data => Debug.Log($"{data.pitch}"));
+            device.StartListen();
+        }
+        
+    }
+}
---
+++ Assets/MimiCast/Scripts/MimiCast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace MimiCast.Scripts
+{
+    public class MimiCast : MonoBehaviour
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FixedUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
